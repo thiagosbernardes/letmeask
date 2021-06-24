72f44d0d3e99531bf85972a309b36f4ad35274d4
Thiago <thiagao_hurricane@hotmail.com>
2021-06-24 14:14:11 -0300

    Quarta aula da nlw together

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,14 +1,16 @@
+import { ReactNode } from 'react'
 import '../styles/question.scss';
 
 type QuestionProps = {
-  content: string,
+  content: string;
   author: {
     name: string;
     avatar: string;
-  }
+  };
+  children?: ReactNode;
 }
 
-export function Question({ content, author }: QuestionProps) {
+export function Question({ content, author, children }: QuestionProps) {
   return (
     <div className="question">
       <p>{content}</p>
@@ -17,7 +19,9 @@ export function Question({ content, author }: QuestionProps) {
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>
+          {children}
+        </div>
       </footer>
     </div>
   );
